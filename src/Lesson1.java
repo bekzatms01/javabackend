659e3af53d59d678a067e310a00bd9a790c21b44
bekzatDev <bekzatmurat2001@gmail.com>
2023-07-18 15:43:31 +0600

    learnt data types

---
+++ src/Lesson1.java
@@ -0,0 +1,28 @@
+import java.lang.reflect.Array;
+
+public class Lesson1 {
+    public static void main(String [] args) {
+        byte value1 = 12;
+        short value2 = 555;
+        int value3 = 4_565_125;
+        long value4 = 5L;
+
+        char ch = 'C';
+        double value5 = 45.6;
+        float value6 = 78.5f;
+        boolean isActor = true;
+
+        int height = 5;
+        int width = 7;
+        int rectangleArea = height * width;
+
+        int val = 44;
+        System.out.println(val);
+
+
+
+
+
+        System.out.println(rectangleArea);
+    }
+}
